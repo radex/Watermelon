337ccce797934b2d878b85b3d52b501a15d5008e
Radex <radex_p [at] tlen [dot] pl>
2008-08-02 18:18:13 +0200

    test commit

---
+++ cms/index.php
@@ -0,0 +1,34 @@
+<?php
+/********************************************************************
+
+  Watermelon CMS
+
+Copyright 2008 Radosław Pietruszewski
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+********************************************************************/
+
+$_w_startTime = microtime();
+session_start();
+ob_start();
+error_reporting(E_ALL ^ E_NOTICE);
+
+//define('NOMENU', '');
+
+include 'config.php';
+
+include WTRMLN_CMSPATH . 'system.php';
+
+?>
