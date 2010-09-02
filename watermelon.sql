aa330eff550c576a791511664f2a377b3dd2791d
Radex <radexpl@gmail.com>
2010-09-02 18:31:33 +0200

    SQL for Benchmarks
    (Chive is nice! phpMyAdmin is so old...)

--- watermelon.sql
+++ watermelon.sql
@@ -1,37 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 3.2.0.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Czas wygenerowania: 10 Sie 2010, 19:32
--- Wersja serwera: 5.1.37
--- Wersja PHP: 5.2.11
+-- 
+-- Structure for table `wm_benchmark`
+-- 
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+DROP TABLE IF EXISTS `wm_benchmark`;
+CREATE TABLE IF NOT EXISTS `wm_benchmark` (
+  `benchmark_name` varchar(256) NOT NULL,
+  `benchmark_value` int(11) NOT NULL,
+  KEY `name` (`benchmark_name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- 
+-- Structure for table `wm_registry`
+-- 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Baza danych: `watermelon`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla  `wm_registry`
---
-
-CREATE TABLE `wm_registry` (
-  `registry_name` varchar(256) NOT NULL,
-  `registry_value` text NOT NULL,
+DROP TABLE IF EXISTS `wm_registry`;
+CREATE TABLE IF NOT EXISTS `wm_registry` (
+  `registry_name` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `registry_value` text CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`registry_name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Zrzut danych tabeli `wm_registry`
---
-
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
