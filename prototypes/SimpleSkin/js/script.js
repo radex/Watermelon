d017560bd7d4a135b03499da81f6c9d70f1bbc48
Radex <radexpl@gmail.com>
2010-10-14 14:20:25 +0200

    "Simple" skin prototype

---
+++ prototypes/SimpleSkin/js/script.js
@@ -0,0 +1,25 @@
+
+function path()
+{
+   var args = arguments, result = [];
+
+   for(var i = 0; i < args.length; i++)
+   {
+      result.push(args[i].replace('@', '../../temp-trash/SyntaxHighlighter/public/brushes/'));
+   }
+
+   return result
+};
+
+SyntaxHighlighter.autoloader.apply(null, path(
+   'cpp c                  @shBrushCpp.js',
+   'css                    @shBrushCss.js',
+   'js jscript javascript  @shBrushJScript.js',
+   'php                    @shBrushPhp.js',
+   'sql                    @shBrushSql.js',
+   'xml xhtml xslt html    @shBrushXml.js'
+));
+
+SyntaxHighlighter.defaults['toolbar'] = false;
+
+SyntaxHighlighter.all();
