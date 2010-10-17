9e3e8cda81092b2efb7864045e7b48139ed70c71
Radex <radexpl@gmail.com>
2010-10-17 15:32:06 +0200

    Refactoring

--- wmelon/packages/test/test.controller.php
+++ wmelon/packages/test/test.controller.php
@@ -10,38 +10,11 @@ class Test_Cache extends FileCache
 
 class test_Controller extends Controller
 {
-   function index_action($foo, $bar, $a = 5)
+   function index_action()
    {
       echo '<a href="$/test/translations">Translations tests</a><br>';
-      echo '<a href="$/test/dbtest">DB tests</a><br>';
       echo '<a href="$/test/cache">Cache tests</a><br>';
-      echo '<a href="$/test/tal">PHPTAL views</a><br>';
-      echo '<a href="$/test/skinviews">Skin views</a><br>';
-      echo '<a href="$/test/user">Logging in, etc.</a><br>';
       echo '<a href="$/test/output">Output types</a><br>';
-      echo '<a href="$/test/css">Boxes, and more...</a><br>';
-      
-      Watermelon::addMessage('tip', 'Foo!');
-   }
-   
-   function css_action()
-   {
-      echo '<div class="errorBox">Ostatnie szlify systemu. Wyższe założenie ideowe, a także wykorzystanie unijnych dotacji pomaga w określaniu odpowiednich warunków aktywizacji. Wyższe założenie ideowe, a szczególnie wdrożenie nowych, lepszych rozwiązań wymaga niezwykłej precyzji w kształtowaniu modelu rozwoju. Przez ostatnie kilkanaście lat odkryliśmy że dokończenie aktualnych projektów jest ważne z powodu systemu szkolenia kadry odpowiadającego.</div>';
-      
-      echo '<div class="warningBox">Co mamy na stałe zabezpieczenie informacyjne naszej działalności koliduje z tym, że rozszerzenie naszej kompetencji w wypracowaniu istniejących kryteriów rozszerza nam horyzonty postaw uczestników wobec zadań programowych wymaga sprecyzowania i realizacji nowych propozycji. Mając na uwadze, że rozszerzenie bazy o nowe rekordy pomaga w restrukturyzacji przedsiębiorstwa. Nie zapominajmy jednak, że.</div>';
-      
-      echo '<div class="infoBox">Wszystko po kolei. Wagi i bogate doświadczenia pozwalają na stałe zabezpieczenie informacyjne naszej kompetencji w przygotowaniu i znaczenia tych problemów nie trzeba udowadniać, ponieważ zmiana istniejących kryteriów ukazuje nam efekt kolejnych kroków w wypracowaniu systemu szkolenia kadry odpowiadającego potrzebom. Nie damy się. Często niezauważanym szczegółem jest nieunikniony. Gdy za 4.</div>';
-      
-      echo '<div class="tickBox">Jednakże, inwestowanie w przyszłościowe rozwiązania wymaga sprecyzowania i realizacji obecnej sytuacji. Wagi i miejsce ostatnimi czasy, dobitnie świadczy o tym, że zakup nowego sprzętu zmusza nas do przeanalizowania nowych propozycji. Różnorakie i określenia form oddziaływania. Mając na rozpoczęcie powszechnej akcji kształtowania podstaw jest to, iż zmiana przestarzałego systemu szkolenia kadr.</div>';
-      
-      echo '<div class="tipBox">Nasza propozycja. W praktyce nowy model działalności powoduje docenianie wag modelu rozwoju. Już nie trzeba udowadniać, ponieważ zakup nowego sprzętu powoduje docenianie wag kierunków postępowego wychowania. Do tej sprawy wymaga sprecyzowania i rozwijanie struktur koliduje z szerokim aktywem jest ważne zadanie w określaniu systemu obsługi pomaga w przygotowaniu i unowocześniania.</div>';
-      
-      echo '<h1>Foo</h1>
-      <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum </p>
-      <h2>Foo</h2>
-      <p>dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus</p>
-      <h3>Foo</h2>
-      <p>Integer ultrices posuere cubilia Curae, Nulla ipsum dolor lacus, suscipit adipiscing. Cum sociis natoque penatibus et ultrices volutpat. Nullam wisi ultricies a, gravida vitae, dapibus risus ante sodales lectus blandit eu, tempor diam pede cursus vitae, ultricies eu, faucibus quis, porttitor eros cursus lectus, pellentesque eget, bibendum a, gravida ullamco</p>';
    }
    
    //--
@@ -71,62 +44,6 @@ class test_Controller extends Controller
    }
    
    //--
-   
-   private function logged()
-   {
-      return (Auth::$isLogged ? 'true' : 'false');
-   } 
-   
-   function user_action()
-   {
-      echo 'Logged:' . $this->logged() . '<br>';
-      
-      echo '<a href="$/test/user_login">Log in</a><br>';
-      echo '<a href="$/test/user_logout">Log out</a><br>';
-   }
-   
-   function user_login_action()
-   {
-      echo 'Logged:' . $this->logged() . '<br>';
-      
-      Auth::login('radex', 'qwerty');
-      
-      echo 'Logged:' . $this->logged() . '<br>';
-   }
-   
-   function user_logout_action()
-   {
-      echo 'Logged:' . $this->logged() . '<br>';
-      
-      Auth::logout();
-      
-      echo 'Logged:' . $this->logged() . '<br>';
-   }
-   
-   //--
-   
-   function skinviews_action()
-   {
-      echo 'Foo:';
-      
-      View('foo')->display();
-      
-      echo '<hr>Bar:';
-      
-      View('bar')->display();
-      
-      echo '<hr>watermelon/bar/foo:';
-      
-      View('watermelon/bar/foo', true)->display();
-   }
-   
-   function tal_action()
-   {
-      $view = View('phptal');
-      $view->foo = 'test';
-      $view->bar = array('a','b');
-      $view->display();
-   }
    
    function cache_action()
    {
@@ -215,20 +132,5 @@ class test_Controller extends Controller
       var_dump(gtr('test', 'He had %1 apples and %2 watermelons', 50, 1050));
       var_dump(gtr('test', 'He liked/didn\'t like watermelons', true));
       var_dump(gtr('test', 'He liked/didn\'t like watermelons', false));
-   }
-   
-   function dbtest_action()
-   {
-      /*
-      $arg1 = '"Blah blah %2"';
-      $arg2 = '"Foo bar %1"';
-      
-      DB::query("%1    %2 fooo %1", $arg1, $arg2);
-      */
-   }
-   
-   function _actionHandler_()
-   {
-      echo str_repeat('actionhnd!', 5);
    }
 }
--- wmelon/packages/watermelon/views/bar/foo.view.php
+++
@@ -1,6 +0,0 @@
-<?die?>
-<div style="border:2px solid green">
-   Hello World, from Second Testing View!<br />
-   It's <?=time()?><br />
-   My path is: <?/*=$this->viewPath*/?>
-</div>
