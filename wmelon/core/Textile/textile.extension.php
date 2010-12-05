8933f5c0a4c4b2a9a897447bd0a093b82694cf45
Radex <radexpl@gmail.com>
2010-12-05 21:24:56 +0100

    Textile improvements
    - <code()></code> shortcut for syntax highlighting
    - <exec></exec> for executing PHP code
    - Textile update

--- wmelon/core/Textile/textile.extension.php
+++ wmelon/core/Textile/textile.extension.php
@@ -22,25 +22,109 @@
  * Textile [A Humane Web Text Generator] extension
  */
 
+/*
+future ideas:
+
+- make it extensible by other extensions
+- and (as an example) make LaTeX extension (that'd be cool, huh?)
+- make it cachable
+
+*/
+
 include 'textile.php';
 
 class Textile_Extension extends Extension
 {
-   private static $textile;
+   private static $textile;         // instance of Textile_Lib
+   private static $shelf = array();
+   
+   /*
+    * public static string textile(string $textile)
+    * 
+    * Converts Textile markup to HTML
+    */
+   
+   public static function textile($text)
+   {
+      // shelving code snippets & PHP to be executed
+      
+      $text = preg_replace_callback('/<code\(([^)]+)\)>(.*?)<\/code>/ms', array(__CLASS__, 'shelveCode'), $text);
+      $text = preg_replace_callback('/<exec>(.*?)<\/exec>/ms', array(__CLASS__, 'shelveExec'), $text);
+      
+      // textile
+      
+      $text = self::$textile->TextileThis($text);
+      
+      // unshelving
+      
+      foreach(self::$shelf as $i => $item)
+      {
+         $text = str_replace('<wm:shelf(' . $i . ')>', $item, $text);
+      }
+      
+      return $text;
+   }
+   
+   /*
+    * public static string textileRestricted(string $textile)
+    * 
+    * Converts restricted Textile markup to HTML
+    * 
+    * Use for comments, forum posts and similar
+    */
+   
+   public static function textileRestricted($text)
+   {
+      return self::$textile->TextileRestricted($text);
+   }
+   
+   /*
+    * (pseudo-)constructor
+    */
    
    public static function onAutoload()
    {
       self::$textile = new Textile_Lib;
    }
    
-   public static function textile($text)
+   /*
+    * replaces <code()>..</code> syntax with placeholder (and puts HTML on "shelf"), so that entities won't be broken by Textile
+    */
+   
+   private static function shelveCode($args)
    {
-      return self::$textile->TextileThis($text);
+      list(, $brush, $code) = $args;
+      
+      $shelvedID = count(self::$shelf);
+      
+      self::$shelf[$shelvedID] = '<pre class="brush: ' . $brush . '">' . htmlspecialchars($code, ENT_NOQUOTES) . '</pre>';
+      
+      return '<wm:shelf(' . $shelvedID . ')>';
    }
    
-   public static function textileRestricted($text)
+   /*
+    * replaces <exec>..</exec> syntax with placeholder (and puts PHP from it on "shelf")
+    */
+   
+   private static function shelveExec($args)
    {
-      return self::$textile->TextileRestricted($text);
+      list(, $code) = $args;
+      
+      // executing code
+      
+      ob_start();
+      
+      $evaluated = eval($code);
+      
+      $evaluated .= ob_get_clean();
+      
+      // shelving
+      
+      $shelvedID = count(self::$shelf);
+      
+      self::$shelf[$shelvedID] = $evaluated;
+      
+      return '<wm:shelf(' . $shelvedID . ')>';
    }
 }
 
