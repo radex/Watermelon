be8e4717b8f4860e17546fa668ae1d705a3a4fa3
Radex <radexpl@gmail.com>
2011-05-02 20:06:15 +0200

    Deleted some experimental installer stuff (maybe one day...)

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
 
-// running Watermelon if installed, installer otherwise
+include 'wmelon/core/Watermelon.php';
 
-if(file_exists(realpath(dirname(__FILE__)) . '/wmelon/core/'))
-{
-   include 'wmelon/core/Watermelon.php';
-   
-   Watermelon::run();
-}
-else
-{
-   include 'wmelon/installer/index.php';
-}
+Watermelon::run();
